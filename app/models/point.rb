93963d79b954a95d7065751e208c9f395d4d555a
RuiMochila <rui.mochila@gmail.com>
2013-06-10 13:26:16 +0100

    Basic set up working. Latitude and Longitude returned from Address given to the point

--- app/models/point.rb
+++ app/models/point.rb
@@ -5,6 +5,7 @@ class Point < ActiveRecord::Base
 
 	def gmaps4rails_address
 	#describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
-  	"#{self.street}, #{self.city}, #{self.country}" 
+  	# "#{self.street}, #{self.city}, #{self.country}" 
+  	"#{self.address}" 
 	end
 end
