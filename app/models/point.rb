16f2b22d76114e4aeaec62491147dc4eaf3991da
RuiMochila <rui.mochila@gmail.com>
2013-06-11 11:51:34 +0100

    removi o geocoder que obrigava a introduzir uma address e calculava o ponto a partir daí

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,7 +1,7 @@
 class Point < ActiveRecord::Base
   attr_accessible :address, :gmaps, :latitude, :longitude, :name, :description
 
-  acts_as_gmappable
+  acts_as_gmappable :process_geocoding => false
 
 	def gmaps4rails_address
 	#describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
